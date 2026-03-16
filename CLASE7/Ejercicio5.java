30634ab28232565ebaa498e8a0dfcb6c39cdc2b3
elizabethhurtadocabezas9-ui <elizabethhurtadocabezas9@gmail.com>
2026-03-16 17:28:43 +0000

    ok

--- CLASE7/Ejercicio5.java
+++ CLASE7/Ejercicio5.java
@@ -4,7 +4,7 @@ public class Ejercicio5 {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("digite numero tabla");
+        System.out.println("digite numero tabla de multiplicar");
         int n = sc.nextInt();
         for (int i = 1; i <= 10; i++) {
             System.out.println(n + "X" + i + "=" + (n * i));
